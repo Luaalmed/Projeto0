48cf0190d6d27106f824b811065e329286c50c86
Luana <luanaa297@gmail.com>
2024-03-19 23:30:28 +0000

    criação do array de tarefas

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
+#include "tarefas.h"
 #include <stdio.h>
-int main(){
+
+int main() {
+  int pos = 0;
+  Tarefa tarefas[TOTAL];
   int opcao;
-  do{
+  do {
     printf("Menu principal\n");
     printf("1 - Criar tarefa\n");
     printf("2 - Deletar tarefa\n");
     printf("3 - Listar tarefas\n");
-    printf( "0- Sair\n");
+    printf("0- Sair\n");
     printf("Digite a opcao desejada: ");
     scanf("%d", &opcao);
-    printf("Opcao escolhida: %d\n",opcao);
-  }while(opcao != 0);
-  }
-
+    printf("Opcao escolhida: %d\n", opcao);
+  } while (opcao != 0);
+}
