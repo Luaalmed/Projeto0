2079169723d10508da91a462e10adfb5d0db470c
Luana Almeida <134015897+Luaalmed@users.noreply.github.com>
2024-03-20 19:31:41 +0000

    chamando as funcoes

--- main.c
+++ main.c
@@ -1,18 +1,30 @@
 #include "tarefas.h"
 #include <stdio.h>
-
 int main() {
-  int pos = 0;
-  Tarefa tarefas[TOTAL];
-  int opcao;
-  do {
-    printf("Menu principal\n");
-    printf("1 - Criar tarefa\n");
-    printf("2 - Deletar tarefa\n");
-    printf("3 - Listar tarefas\n");
-    printf("0- Sair\n");
-    printf("Digite a opcao desejada: ");
-    scanf("%d", &opcao);
-    printf("Opcao escolhida: %d\n", opcao);
-  } while (opcao != 0);
+int pos = 0;
+Tarefa tarefas[TOTAL];
+
+int opcao;
+do {
+printf("\nMenu principal\n");
+printf("1- Criar tarefas\n");
+printf("2- Deletar tarefas\n");
+printf("3- Listar tarefas\n");
+printf("0- Sair\n:");
+printf("Entre com a opcao:");
+int i = scanf("%d", &opcao);
+printf("Opcao escolhida: %d\n", opcao);
+
+if (opcao == 1){
+int erro = criar(tarefas, &pos);
+} else if (opcao == 2){
+int erro = deletar(tarefas, &pos);
+} else if (opcao == 3){
+int erro = listar(tarefas, pos);
+} else if (opcao==0){
+} else {
+printf("Sair\n");
+}
+} while (opcao != 0);
+
 }
---
+++ tarefas.c
@@ -0,0 +1,26 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+int criar(Tarefa tarefas[], int *pos) {
+  printf("funcao de criar tarefas\n");
+  return 0;
+}
+
+int deletar(Tarefa tarefas[], int *pos) {
+  printf("funcao de deletar tarefas\n");
+  return 0;
+}
+int listar(Tarefa tarefas[], int pos) {
+  printf("funcao de listar tarefas\n");
+  return 0;
+}
+
+int salvar(Tarefa tarefas[], int total, int pos) {
+  printf("funcao de ssalvar tarefas\n");
+  return 0;
+}
+
+int carregar(Tarefa tarefas[], int total, int pos) {
+  printf("funcao de carregar tarefas\n");
+  return 0;
+}
